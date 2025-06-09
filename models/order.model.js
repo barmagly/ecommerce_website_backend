d50c9c4439e1587327496c54f84810df0ca45c66
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-06-09 17:26:09 +0300

    handel order

--- models/order.model.js
+++ models/order.model.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
+
 const orderItemSchema = new mongoose.Schema({
     product: {
         type: mongoose.Schema.ObjectId,
         ref: 'Product',
         required: true
     },
+    variantId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'ProductVariant',
+        required: false
+    },
     quantity: {
         type: Number,
         required: true,
-    }
-})
+    },
+    name: String,
+    price: Number,
+    image: String
+});
 
 const OrderSchema = new mongoose.Schema({
     total: Number,
-    items: [orderItemSchema],
+    cartItems: [orderItemSchema], 
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
@@ -33,6 +42,27 @@ const OrderSchema = new mongoose.Schema({
         default: false,
     },
     deliveredAt: Date,
+    name: String,
+    phone: String,
+    address: String,
+    city: String,
+    postalCode: String,
+    country: String,
+    email: String,
+    image: {
+        type: String,
+        default: ''
+    },
+    paymentMethod: {
+        type: String,
+        enum: ["credit_card", "debit_card", "paypal", "bank_transfer", "cash_on_delivery"],
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["pending", "completed", "failed", "refunded"],
+        default: "pending"
+    }
 }, { timestamps: true });
 
 const OrderModel = mongoose.model('order', OrderSchema);
